100923ca8859c0abe9265f58eb5be2bbd4fba606
Rayhan-Rony <jobayerahmedrony@gmail.com>
2025-03-01 14:06:51 +0600

    added functionality for history

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
 const completeBtn=document.querySelectorAll(".btn-completed-task")
 
+
 for (const btn of completeBtn){
    
     btn.addEventListener("click",function(){
-
+// alert 
         alert("Board Updated Successfully")
+        // get and set text for assigned task 
         const assignTask=getInnerTextById("assigned-task-count")
         setInnerText("assigned-task-count",`0${assignTask-1}`)
+        // disable the button after click 
         this.disabled=true;
         this.classList.add("bg-gray-300")
+        // get and set text for nav checkbox 
         const completedTaskCheckboxText=getInnerTextById("completed-task-checkbox-text")
         setInnerText("completed-task-checkbox-text",`${completedTaskCheckboxText+1}`)
+        // accessing title  of this card 
+       const title=this.parentNode.parentNode.parentNode.querySelector(".pending-task-title")
+    //    accessing current time 
+     const time=getTime()
+       
+        // create element for history  and appending
+       const p= document.createElement("p")
+       p.innerText=`You have Complete The Task ${title.innerText} at ${time}`
+       p.classList.add("my-8","bg-[#f4f7ff]","mx-5","p-3","rounded-lg")
+       document.getElementById("task-history").appendChild(p)
+       
+      
+
         
         
     })
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -7,3 +7,14 @@ function setInnerText(id,text){
     document.getElementById(id).innerText=text;
 
 }
+function getTime(){
+    const time = new Date();
+        // let hour=time.getHours()
+        // const minute=time.getMinutes()
+        // const second=time.getSeconds()
+        // const amPm =hour<=12?"am":"pm"
+        // converting 12 Hour Formate 
+        // hour = hour % 12 || 12
+        // return `${hour}:${minute}:${second}${amPm}`
+        return time.toLocaleTimeString(); 
+}
